8f31efaffdb55aeea08e2cae9e36d655050c7a99
Martin Swiderski <martinswiderski@googlemail.com>
2018-09-17 17:15:37 +0100

    wip

--- bash/java-oracle.sh
+++ bash/java-oracle.sh
@@ -1,5 +1,43 @@
 #!/usr/bin/env bash
 
+MANIFEST='manifest.txt'
+URL='https://raw.githubusercontent.com/ibuprofan/dev-setup/master/bin/'
+JDK='jdk-8u181-linux-x64.tar.gz'
+JRE='jre-8u181-linux-x64.tar.gz'
+JDK_DIR='jdk1.8.0_181'
+JRE_DIR='jre1.8.0_181'
+MY_VER=''
+MY_DIR=''
+
+echo ""
+echo " +------------------------------------------------+"
+echo " | Oracle Java:                                   |"
+echo " |                                                |"
+echo " |  (1). JDK 1.8: ${JDK}      |"
+echo " |  (2). JRE 1.8: ${JRE}      |"
+echo " +------------------------------------------------+"
+echo ""
+echo " Choose version to install..."
+echo ""
+stty -echo
+read VERSION
+stty echo
+
+if [ "${VERSION}" = "1" ]; then
+  MY_VER="${JDK}"
+  MY_DIR="${JDK_DIR}"
+elif [ "${VERSION}" = "2" ]; then
+  MY_VER="${JRE}"
+  MY_DIR="${JRE_DIR}"
+else
+  echo "Invalid choice: ${VERSION}"
+  exit 1
+fi
+
+echo "Installing ${MY_VER}..."
+
+exit 0
+
 # remove openJDK
 sudo apt-get update && apt-get remove openjdk-6-jre
 sudo apt-get autoremove && apt-get clean
@@ -14,4 +52,3 @@ sudo update-alternatives --install "/usr/bin/java" "java" "/opt/java/64/jdk1.8.0
 sudo update-alternatives --set java /opt/java/64/jdk1.8.0_181/bin/java
 # Installed?
 java -version
-
